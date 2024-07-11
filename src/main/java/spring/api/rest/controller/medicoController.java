a47cb6c84008513acd3dc67b3018b2a2bd981e20
matheus freitas <mgsoutodelira@gmail.com>
2024-07-11 10:53:58 -0300

    create project

---
+++ src/main/java/spring/api/rest/controller/medicoController.java
@@ -0,0 +1,14 @@
+package spring.api.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class medicoController {
+    @GetMapping
+    public String hello() {
+        return "Hello World, Spring!";
+    }
+}
